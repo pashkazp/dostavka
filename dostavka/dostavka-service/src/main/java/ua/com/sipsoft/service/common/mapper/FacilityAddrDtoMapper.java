b0be7bac130ff59fdea9aa04ef4fac4a2ce3c221
DePavlo <pashka@ukr.net>
2021-02-08 15:08:30 +0200

    improve blank string to double conversion

--- dostavka/dostavka-service/src/main/java/ua/com/sipsoft/service/common/mapper/FacilityAddrDtoMapper.java
+++ dostavka/dostavka-service/src/main/java/ua/com/sipsoft/service/common/mapper/FacilityAddrDtoMapper.java
@@ -1,6 +1,9 @@
 package ua.com.sipsoft.service.common.mapper;
 
+import org.apache.commons.lang3.StringUtils;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.springframework.stereotype.Component;
 
 import ua.com.sipsoft.service.dto.facility.FacilityAddrDto;
@@ -10,6 +13,13 @@ import ua.com.sipsoft.service.dto.facility.FacilityAddrRegReqDto;
 @Component
 public interface FacilityAddrDtoMapper {
 
+	@Mapping(source = "lat", target = "lat", qualifiedByName = "toDoubleTransformation")
+	@Mapping(source = "lng", target = "lng", qualifiedByName = "toDoubleTransformation")
 	public abstract FacilityAddrDto toDto(FacilityAddrRegReqDto facilityAddrRegDto);
+
+	@Named("toDoubleTransformation")
+	default Double myCustomTransformation(String obj) {
+		return StringUtils.isBlank(obj) ? null : Double.valueOf(obj);
+	}
 
 }
