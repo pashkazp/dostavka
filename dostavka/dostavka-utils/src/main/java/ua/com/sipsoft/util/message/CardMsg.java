b51476da347c711ab289676ce90684c1f61a5e00
DePavlo <pashka@ukr.net>
2021-02-13 10:47:53 +0200

    Change naming for some Cards

---
+++ dostavka/dostavka-utils/src/main/java/ua/com/sipsoft/util/message/CardMsg.java
@@ -0,0 +1,20 @@
+package ua.com.sipsoft.util.message;
+
+/**
+ * Set of standard messages for Application Cards.
+ *
+ * @author Pavlo Degtyaryev
+ */
+public class CardMsg {
+	public static final String USERS = "card.users";
+	public static final String FACILITIES = "card.facilities";
+	public static final String DRAFT_SHEETS = "card.draft.sheets";
+	public static final String DRAFT_SHEETREQS = "card.draft.sheetreqs";
+	public static final String DRAFT_REQS = "card.draft.reqs";
+
+	/**
+	 * Instantiates a new App cards msg.
+	 */
+	private CardMsg() {
+	}
+}
